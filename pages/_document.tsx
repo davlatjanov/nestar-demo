0a07f8d67fc32c7a53f0ad57787b0f9881df7f94
davlatjanov <ndavlatjonov@gmail.com>
2026-01-25 19:58:53 +0900

    feat: meta tags integrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTD-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        <meta
+          name="keyword"
+          content={"nestar , nestar.uz , devex mern , mern nestjs fullstack"}
+        />
+
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere anytime in South Korea |" +
+            "한국 어디에서든 언제든지 부동산을 사고팔 수 있습니다 |" +
+            "Покупка и продажа недвижимости в Южной Корее в любое время и в любом месте"
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
