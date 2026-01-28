a812b62e640593b78a089d036afa2d2f8d54ee1d
davlatjanov <ndavlatjonov@gmail.com>
2026-01-28 14:41:26 +0900

    fix: refactor pages components

---
+++ pages/agent/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+import { Stack } from "@mui/material";
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+
+const AgentList: NextPage = () => {
+  return (
+    <div
+      style={{
+        margin: "20px 0",
+      }}
+    >
+      <Stack className={"container"}> AGENT LIST </Stack>
+    </div>
+  );
+};
+
+export default withLayoutBasic(AgentList);
--- pages/cs/index.tsx
+++ pages/cs/index.tsx
@@ -2,16 +2,16 @@ import { NextPage } from "next";
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 import { Stack } from "@mui/material";
 
-const Community: NextPage = () => {
+const CS: NextPage = () => {
   return (
     <div
       style={{
         margin: "20px 0",
       }}
     >
-      <Stack className={"container"}> COMMUNITY </Stack>
+      <Stack className={"container"}> CS </Stack>
     </div>
   );
 };
 
-export default withLayoutBasic(Community);
+export default withLayoutBasic(CS);
